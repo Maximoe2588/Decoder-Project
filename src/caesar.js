212d1611369e149f251e854fd5ffb28cc1e57010
Maximo Mora <70680274+Maximoe2588@users.noreply.github.com>
2023-03-14 17:40:02 -0500

    Update caesar.js
    Removed Comments

--- src/caesar.js
+++ src/caesar.js
@@ -1,19 +1,16 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
+
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+  
 
   function caesar(input, shift, encode = true) {
-    //if statement for defaults
+    
     if(!shift || shift < -25 || shift > 25) return false
     if(!encode) shift *=-1
       return input.toLowerCase().split('').map((letter)=> {
-        //if statements using ascii table 
+       
         if(letter.charCodeAt(0) <97) return letter
         let num = letter.charCodeAt(0) + parseInt(shift)
-        //if statements making sure number stays in ascii table for alphabet
+     
         if (num > 122) {
           num = 96 + (num - 122)
         }
